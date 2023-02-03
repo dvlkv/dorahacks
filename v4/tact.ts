a5b43afb8c16f2a08cbbcb5aa04ad891cb82ae57
mrmld <die.marmelade@yandex.ru>
2023-02-03 12:44:36 +0400

    wip: add tact example

--- v4/tact.ts
+++ v4/tact.ts
@@ -1,13 +1,13 @@
 import { Address, beginCell, storeStateInit } from 'ton-core';
 import { jettonContractAddress } from '../tact/address';
 import { SampleJetton } from '../tact/output/jetton_SampleJetton';
-import { toncenter } from './client';
+import { client4 } from './client';
 
 const address = Address.parse('EQA-daKmzkx5nLMKT465D_-uyhwgBTEucMeyvfGLfzHoWspv');
 
 (async () => {
     let jettonContractSource = SampleJetton.fromAddress(jettonContractAddress);
-    let jettonContract = toncenter.open(jettonContractSource);
+    let jettonContract = client4.open(jettonContractSource);
 
     console.log('Owner:', await jettonContract.getOwner());
     console.log('Your wallet:', await jettonContract.getGetWalletAddress(address));
